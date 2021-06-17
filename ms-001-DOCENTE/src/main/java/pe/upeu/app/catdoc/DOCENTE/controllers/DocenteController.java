1e06741fee8f42a42df4aeb5a86083feff03fc3f
christopherdobson <christopherdobson@upeu.edu.pe>
2021-06-16 23:52:47 -0500

    add docente controller

--- ms-001-DOCENTE/src/main/java/pe/upeu/app/catdoc/DOCENTE/controllers/DocenteController.java
+++ ms-001-DOCENTE/src/main/java/pe/upeu/app/catdoc/DOCENTE/controllers/DocenteController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +37,30 @@ public class DocenteController {
 	public Docente crear(@RequestBody Docente docente) {
 		return docenteService.save(docente);
 			
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Docente editar(@RequestBody Docente docente, @PathVariable Integer id) {
+		Docente docenteDb = docenteService.findById(id);
+		docenteDb.setNombre(docente.getNombre());
+		docenteDb.setApMaterno(docente.getApMaterno());
+		docenteDb.setApPaterno(docente.getApPaterno());
+		docenteDb.setCampus(docente.getCampus());
+		docenteDb.setCatIdCat(docente.getCatIdCat());
+		docenteDb.setClave(docente.getClave());
+		docenteDb.setCorreo(docente.getCorreo());
+		docenteDb.setDni(docente.getDni());
+		docenteDb.setEstado(docente.getEstado());
+		docenteDb.setFechaNacimiento(docente.getFechaNacimiento());
+		docenteDb.setSexo(docente.getSexo());
+		docenteDb.setUsuario(docente.getUsuario());
+		return docenteService.save(docenteDb);
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Integer id) {
+		docenteService.deleteById(id);
 	}
 }
